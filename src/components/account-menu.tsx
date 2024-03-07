289e33c25009ffb71dc49794ebe032f08aa321a4
Aldinei <aldinei.franco@unimedprudente.com.br>
2024-03-07 14:22:36 -0300

    finalizando dropdow menu

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,25 +1,36 @@
-import { ChevronDown } from "lucide-react";
+import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
-import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "./ui/dropdown-menu";
 
 
 export function AccountMenu() {
   return (
     <DropdownMenu>
-      
+
       <DropdownMenuTrigger asChild>
         <Button variant='outline' className="flex items-center gap-2 select-none">
           Pizza Shop
-          <ChevronDown className="h-4 w-4"/>
+          <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
-      
+
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col p-2">
           <span>Aldinei Franco</span>
           <span className="text-xs font-normal text-muted-foreground">aldineifranco@gmail.com</span>
         </DropdownMenuLabel>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem>
+          <Building className="w-4 h-4 mr-2" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span>Sair</span>
+        </DropdownMenuItem>
       </DropdownMenuContent>
 
     </DropdownMenu>
