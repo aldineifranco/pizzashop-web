336beaa5436881d253e13ce7622e7efacaff08fd
Aldinei <aldinei.franco@unimedprudente.com.br>
2024-03-25 08:11:37 -0300

    requisições dodos gerente e dados empresa. getProfile e getManagedRestaurant

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -7,6 +7,8 @@ import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { registerRestaurant } from '@/api/register-restaurant'
+import { useMutation } from '@tanstack/react-query'
 
 
 
@@ -23,15 +25,23 @@ export function SignUp() {
   const navigate = useNavigate()
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignUpForm>()
 
+  const { mutateAsync: registerRestaurantFn } = useMutation({
+    mutationFn: registerRestaurant,
+  })
+
   async function handleSignUp(data: SignUpForm) {
     try {
-      await new Promise(resolve => setTimeout(resolve, 2000))
-      console.log(data)
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        managerName: data.managerName,
+        email: data.email,
+        phone: data.phone,
+      })
 
       toast.success('Restaurante cadastrado com sucesso!', {
         action: {
           label: 'Login',
-          onClick: () => navigate('/sign-in'),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         }
       })
     } catch {
