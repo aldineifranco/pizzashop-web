1ef344bc5d113726c46a1be7bf3af6bca188192e
Aldinei <aldinei.franco@unimedprudente.com.br>
2024-04-03 08:55:57 -0300

    página orders - integração com backend

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -3,14 +3,21 @@ import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders
+  })
+
   return (
     <>
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tighter">Pedidos</h1>
-
+      
         <div className="space-y-2.5">
           <OrderTableFilters />
 
@@ -30,16 +37,14 @@ export function Orders() {
               </TableHeader>
 
               <TableBody>
-                {Array.from({ length: 10 }).map((_, i) => {
-                  return (
-                    <OrderTableRow key={i} />
-                  )
+                {result && result.orders.map(order => {
+                  return <OrderTableRow key={order.orderId} order={order} />
                 })}
               </TableBody>
             </Table>
           </div>
 
-        <Pagination pageIndex={0} totalCount={105} perPage={10} />        
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
